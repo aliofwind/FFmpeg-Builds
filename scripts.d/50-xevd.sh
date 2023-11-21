1636edffdf2c5f522194bd724d52c997c89a103d
aliofwind <35068322+aliofwind@users.noreply.github.com>
2023-11-21 09:35:54 +0800

    add evc

---
+++ scripts.d/50-xevd.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/mpeg5/xeve.git"
+SCRIPT_COMMIT="cebfd5c0350ebcab0e08840af3a35b5a2773563b"
+
+ffbuild_enabled() {
+    [[ $TARGET == win32 ]] && return -1
+    return 0
+}
+
+ffbuild_dockerdl() {
+    to_df "RUN git clone \"$SCRIPT_REPO\" \"$SELF\" && git -C \"$SELF\" checkout \"$SCRIPT_COMMIT\""
+}
+
+ffbuild_dockerbuild() {
+    cd "$FFBUILD_DLDIR/$SELF"
+
+    mkdir build && cd build
+
+    cmake .. -G "MinGW Makefiles"
+    make -j$(nproc)
+    make install
+}
+
+ffbuild_configure() {
+    echo --enable-libxeve
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libxeve
+}
---
+++ scripts.d/50-xeve.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/mpeg5/xevd.git"
+SCRIPT_COMMIT="418ed6dfb5d09b9c730f0d474364436d994f4006"
+
+ffbuild_enabled() {
+    [[ $TARGET == win32 ]] && return -1
+    return 0
+}
+
+ffbuild_dockerdl() {
+    to_df "RUN git clone \"$SCRIPT_REPO\" \"$SELF\" && git -C \"$SELF\" checkout \"$SCRIPT_COMMIT\""
+}
+
+ffbuild_dockerbuild() {
+    cd "$FFBUILD_DLDIR/$SELF"
+
+    mkdir build && cd build
+
+    cmake .. -G "MinGW Makefiles"
+    make -j$(nproc)
+    make install
+}
+
+ffbuild_configure() {
+    echo --enable-libxeve
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libxeve
+}
