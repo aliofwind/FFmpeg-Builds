cc85e175d5c51e511d58d733d578690a60fa6374
aliofwind <35068322+aliofwind@users.noreply.github.com>
2023-11-21 09:50:17 +0800

    fix xeve/d

--- scripts.d/50-xeve.sh
+++ scripts.d/50-xeve.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/mpeg5/xevd.git"
-SCRIPT_COMMIT="418ed6dfb5d09b9c730f0d474364436d994f4006"
+SCRIPT_COMMIT="cebfd5c0350ebcab0e08840af3a35b5a2773563b"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1
