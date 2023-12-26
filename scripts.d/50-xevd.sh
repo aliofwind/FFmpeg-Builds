9084e506447e9ec9475d21af81ec16d1ca5462dd
aliofwind <35068322+aliofwind@users.noreply.github.com>
2023-12-26 21:52:50 +0800

    fix

--- scripts.d/50-xevd.sh
+++ scripts.d/50-xevd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/mpeg5/xevd.git"
-SCRIPT_COMMIT="418ed6dfb5d09b9c730f0d474364436d994f4006"
+SCRIPT_COMMIT="4e76654c5f2595d8e24d7577ebe9b92cdfdb442f"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1
--- scripts.d/50-xeve.sh
+++ scripts.d/50-xeve.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/mpeg5/xeve.git"
-SCRIPT_COMMIT="cebfd5c0350ebcab0e08840af3a35b5a2773563b"
+SCRIPT_COMMIT="3890dae66c6b720dd3ffd53f31dbae80c48d7c5b"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1
