81ba2185a25b252af7166c8d84af326f315fe55d
aliofwind <35068322+aliofwind@users.noreply.github.com>
2026-03-08 11:09:12 +0800

    use avs branch

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ mkdir ffbuild
 
 FFMPEG_REPO="${FFMPEG_REPO:-https://github.com/aliofwind/FFmpeg.git}"
 FFMPEG_REPO="${FFMPEG_REPO_OVERRIDE:-$FFMPEG_REPO}"
-GIT_BRANCH="${GIT_BRANCH:-master}"
+GIT_BRANCH="${GIT_BRANCH:-avs}"
 GIT_BRANCH="${GIT_BRANCH_OVERRIDE:-$GIT_BRANCH}"
 
 BUILD_SCRIPT="$(mktemp)"
